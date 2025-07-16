4e92a13657d232d328af229504e3193e31e1aaf5
afw238 <chqwulmin0814@gmail.com>
2025-07-16 17:11:25 +0900

    planet growing
    식물이 자라요

---
+++ Assets/Branches/PCM/SO/GrowingSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GrowingSO", menuName = "Scriptable Objects/GrowingSO")]
+public class GrowingSO : ScriptableObject
+{
+    public int time;
+    public Sprite[] sprites;
+
+}
